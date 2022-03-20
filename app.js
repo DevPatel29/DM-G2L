17b3835f43f609ad49d454924c0cece46e7d8836
DevPatel29 <devmpatel29@gmail.com>
2022-03-20 15:56:11 +0530

    show table for seleceted attributes from a single table, without any join

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const port = 3000;
 const fs = require('fs');
 
 
+app.use(express.static('src'));
+app.use(express.json());
+
+app.get('', (req, res) =>{
+    res.sendFile(__dirname + '/src/index.html');
+})
+
+
 app.get('/xmldata-api', (req, res) => {
     fs.readFile('./src/global_schema.xml', 'utf8', (err,data) =>{
         if(err){
@@ -14,12 +22,38 @@ app.get('/xmldata-api', (req, res) => {
     });
 });
 
-app.use(express.static('src'));
+app.get('/LS_gis', (req, res) => {
+    fs.readFile('./src/LS_gis.xml', 'utf8', (err,data) =>{
+        if(err){
+            console.error(err);
+            return;
+        }
+        res.json({ text: data });
+    });
+});
 
-app.get('', (req, res) =>{
-    res.sendFile(__dirname + '/src/index.html');
-})
+app.get('/LS_route', (req, res) => {
+    fs.readFile('./src/LS_route.xml', 'utf8', (err,data) =>{
+        if(err){
+            console.error(err);
+            return;
+        }
+        res.json({ text: data });
+    });
+});
 
+
+app.post('/csvdata-api', (req,res)=>{
+    var path = req.body.path;
+    // console.log(path);
+    fs.readFile(path, 'utf8', (err,data) =>{
+        if(err){
+            console.error(err);
+            return;
+        }
+        res.json({ text: data });
+    });
+});
 
 
 app.listen(port, () => console.info(`Listening on port ${port}`));
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 fetch('http://localhost:3000/xmldata-api')
     .then(response => {return response.json()})
     .then(data => {
-        var temp = document.getElementById("xmlDoc");
+        var temp = document.getElementById("gs_xmlDoc");
         temp.innerHTML += data.text;
         setup();
     })
     .catch(err => console.log(err));
 
-var xml_data;
-var xmlDom;
+
 var ddlDB = document.getElementById("ddlDatabase");
 var ddlEntities = document.getElementById('ddlEntities');
 var ddlAttributesCheckBox = document.getElementById('ddlAttributesCheckBox');
+var global_schema;
 
 var selectedAttributes = new Set();
 
-function setup(){
-    xml_data = document.getElementById("xmlDoc").textContent;
 
+function get_XML_Dom_object(xml_data){
+    var xmlDom;
     if (window.DOMParser) {
         parser = new DOMParser();
         xmlDom = parser.parseFromString(xml_data, "text/xml");
@@ -27,6 +27,13 @@ function setup(){
         xmlDom.async = false;
         xmlDom.loadXML(xml_data);
     }
+
+    return xmlDom;
+}
+
+function setup(){
+    var xml_data = document.getElementById("gs_xmlDoc").textContent;
+    global_schema = get_XML_Dom_object(xml_data).getElementsByTagName("global_schema");
 }
 
 
@@ -42,8 +49,6 @@ function PopulateDatabase() {
     removeOptions(ddlEntities);
     document.getElementById('rightColumnDiv').innerHTML = "";
     
-    var global_schema = xmlDom.getElementsByTagName("global_schema");
-
     for (var i = 0; i < global_schema.length; i++) {
         var option = document.createElement("option");
 
@@ -58,8 +63,6 @@ function PopulateDatabase() {
 function PopulateEntities(){
     removeOptions(ddlEntities);
     document.getElementById('rightColumnDiv').innerHTML = "";
-
-    var global_schema = xmlDom.getElementsByTagName("global_schema");
 
     for (var i = 0; i < global_schema.length; i++) {
         if(global_schema[i].getElementsByTagName("global_schema_id")[0].textContent == ddlDB.value){
@@ -77,8 +80,6 @@ function PopulateEntities(){
 
 function PopulateAttributes(){
     document.getElementById('rightColumnDiv').innerHTML = "";
-
-    var global_schema = xmlDom.getElementsByTagName("global_schema");
     var container = document.getElementById('rightColumnDiv');
 
     for (var i = 0; i < global_schema.length; i++) {
@@ -112,13 +113,12 @@ function PopulateAttributes(){
     let btn = document.createElement("button");
     btn.innerHTML = "Add";
     btn.style.marginTop = "30px";
-    btn.onclick = function() {addAttributeToList()};
+    btn.onclick = function() {addAttributesToList()};
     container.appendChild(btn);
 }
 
-function addAttributeToList(){
+function addAttributesToList(){
     var childCB = document.getElementById('rightColumnDiv').getElementsByTagName('input');
-    var curr
 
     for( i=0; i< childCB.length; i++ ){
         if(childCB[i].checked){
@@ -129,6 +129,117 @@ function addAttributeToList(){
             }
         }
     }
+}
 
-    console.log(selectedAttributes);
+async function showTable(){
+    document.getElementById('tableDisplayDiv').innerHTML = "";
+    var ls_text = await get_local_schema();
+    var ls = get_XML_Dom_object(ls_text);
+
+    var path = ls.getElementsByTagName("manifest")[0].getElementsByTagName("description")[0].getElementsByTagName("static")[0].getElementsByTagName("source_api")[0].textContent;
+    var entity = ls.getElementsByTagName("metadata")[0].getElementsByTagName("entity");
+    for(var i=0;i<entity.length;i++){
+        if(entity[i].getElementsByTagName("eid")[0].textContent == ddlEntities.value){
+            path += entity[i].getElementsByTagName("data_source")[0].textContent;
+        }
+    }
+
+    const options = {
+        method : 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({path})
+    };
+
+    const data = await fetch('/csvdata-api',options)
+                       .then(response => {return response.json()})
+
+    const tableData = processData(data.text);
+    createTable(tableData.header, tableData.lines);
+}
+
+function processData(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var header = allTextLines[0].split(',');
+    var lines = [];
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == header.length) {
+
+            var tarr = [];
+            for (var j=0; j<header.length; j++) {
+                tarr.push(data[j]);
+            }
+            lines.push(tarr);
+        }
+    }
+
+    return {header, lines};
+}
+
+function createTable(tableHeader, tableData){
+    var table = document.createElement('table');
+    table.id = "displayTable";
+    var tableBody = document.createElement('tbody');
+
+    var anyBoxesChecked = new Array(tableHeader.length).fill(false);
+
+    {
+        var row = document.createElement('tr');
+
+        for(var i=0;i<tableHeader.length;i++){
+            var temp = ddlDB.value + '.' + ddlEntities.value + '.' + tableHeader[i];
+            if(selectedAttributes.has(temp)){
+                anyBoxesChecked[i] = true;
+            }
+
+            if(anyBoxesChecked[i]){
+                var cell = document.createElement('th');
+                cell.appendChild(document.createTextNode(tableHeader[i]));
+                row.appendChild(cell);
+            }
+        }
+
+        tableBody.appendChild(row);
+    }
+  
+    tableData.forEach(function(rowData) {
+        var row = document.createElement('tr');
+
+        for(var i=0;i<rowData.length;i++){
+            if(anyBoxesChecked[i]){
+                var cell = document.createElement('td');
+                cell.appendChild(document.createTextNode(rowData[i]));
+                row.appendChild(cell);
+            }
+        }
+    
+        tableBody.appendChild(row);
+    });
+  
+    table.appendChild(tableBody);
+    document.getElementById('tableDisplayDiv').appendChild(table);
+}
+
+async function get_local_schema(){
+    var ls_path;
+    for (var i = 0; i < global_schema.length; i++) {
+        if(global_schema[i].getElementsByTagName("global_schema_id")[0].textContent == ddlDB.value){
+            var entities = global_schema[i].getElementsByTagName("entity");
+
+            for(var j = 0;j < entities.length;j++){
+                if(entities[j].getElementsByTagName("eid")[0].textContent == ddlEntities.value){
+                    ls_path = entities[j].getElementsByTagName("ls_path")[0].textContent;    
+                }
+            }
+        }
+    }
+
+    var data = await fetch(ls_path)
+                        .then(response => {return response.json()});
+    var ls_text = data.text;
+
+    return ls_text;
 }
