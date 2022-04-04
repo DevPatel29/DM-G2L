e73bd9e03123efdfded382b46078c3f11bf042b6
DevPatel29 <devmpatel29@gmail.com>
2022-04-04 15:47:23 +0530

    joining table from direct relationship

--- src/index.js
+++ src/index.js
@@ -14,6 +14,12 @@ var ddlAttributesCheckBox = document.getElementById('ddlAttributesCheckBox');
 var global_schema;
 
 var selectedAttributes = new Set();
+var selectedEntities = new Set();
+
+var graph = {
+    'dsid2_eid2_route_no' : ['dsid2_eid3_route_no'],
+    'dsid2_eid3_route_no' : ['dsid2_eid2_route_no']
+};
 
 
 function get_XML_Dom_object(xml_data){
@@ -121,36 +127,52 @@ function addAttributesToList(){
 
     for( i=0; i< childCB.length; i++ ){
         if(childCB[i].checked){
-            var temp = ddlDB.value + '.' + ddlEntities.value + '.' + childCB[i].name;
+            var temp = ddlDB.value + '_' + ddlEntities.value + '_' + childCB[i].name;
+            selectedEntities.add((ddlDB.value + '-' + ddlEntities.value));
             if(!selectedAttributes.has(temp)){
                 selectedAttributes.add(temp);
                 document.getElementById('attributesDisplayDiv').innerHTML += childCB[i].name + " ";
             }
         }
     }
+    // Temp();
 }
 
 async function showTable(){
     document.getElementById('tableDisplayDiv').innerHTML = "";
-    var lsText = await get_local_schema();
-    var ls = get_XML_Dom_object(lsText);
 
-    if(ls.getElementsByTagName("manifest")[0].getElementsByTagName("type")[0].textContent == "CSV_data"){
-        ProcessCSV(ls);
+    const arrayTable = [];
+    console.log(selectedEntities);
+    for(let ele of selectedEntities){
+        const dsID = ele.split("-")[0];
+        const eID = ele.split("-")[1];
+
+        var lsText = await get_local_schema(dsID,eID);
+        var ls = get_XML_Dom_object(lsText);
+
+        if(ls.getElementsByTagName("manifest")[0].getElementsByTagName("type")[0].textContent == "CSV_data"){
+            const data = await ProcessCSV(ls, eID);
+            arrayTable.push(data);
+        }
+        else if(ls.getElementsByTagName("manifest")[0].getElementsByTagName("type")[0].textContent == "SQL_data"){
+            const data = await ProcessSQL(ls);
+            arrayTable.push(data);
+        }
     }
-    else if(ls.getElementsByTagName("manifest")[0].getElementsByTagName("type")[0].textContent == "SQL_data"){
-        ProcessSQL(ls);
-    }
+
+    console.log(arrayTable);
+    ProcessArrayTable(arrayTable);
 }
 
-async function get_local_schema(){
+// return array of lsText;
+async function get_local_schema(dsID, eID){
     var ls_path;
     for (var i = 0; i < global_schema.length; i++) {
-        if(global_schema[i].getElementsByTagName("global_schema_id")[0].textContent == ddlDB.value){
+        if(global_schema[i].getElementsByTagName("global_schema_id")[0].textContent == dsID){
             var entities = global_schema[i].getElementsByTagName("entity");
 
             for(var j = 0;j < entities.length;j++){
-                if(entities[j].getElementsByTagName("eid")[0].textContent == ddlEntities.value){
+                if(entities[j].getElementsByTagName("eid")[0].textContent == eID){
                     ls_path = entities[j].getElementsByTagName("ls_path")[0].textContent;    
                 }
             }
@@ -164,11 +186,12 @@ async function get_local_schema(){
     return lsText;
 }
 
-async function ProcessCSV(ls){
+//return the data but first change coloumn name to db.entitiy.column name
+async function ProcessCSV(ls, eID){
     var path = ls.getElementsByTagName("manifest")[0].getElementsByTagName("description")[0].getElementsByTagName("static")[0].getElementsByTagName("source_api")[0].textContent;
     var entity = ls.getElementsByTagName("metadata")[0].getElementsByTagName("entity");
     for(var i=0;i<entity.length;i++){
-        if(entity[i].getElementsByTagName("eid")[0].textContent == ddlEntities.value){
+        if(entity[i].getElementsByTagName("eid")[0].textContent == eID){
             path += entity[i].getElementsByTagName("data_source")[0].textContent;
         }
     }
@@ -181,10 +204,22 @@ async function ProcessCSV(ls){
         body: JSON.stringify({path})
     };
 
-    const data = await fetch('/csvdata-api',options)
+    let data = await fetch('/csvdata-api',options)
                        .then(response => {return response.json()})
 
-    CreateTableFromJson(data);
+    // CreateTableFromJson(data);
+
+    let pref = ls.getElementsByTagName("manifest")[0].getElementsByTagName("data_source_id")[0].textContent + '_' + eID + '_';
+    data = data.map((elm) => {
+        var res = {};
+        for(k in elm){
+            var temp = pref + k;
+            res[temp] = elm[k];
+        }
+        return res;
+    });
+
+    return data;
 }
 
 async function ProcessSQL(ls){
@@ -224,7 +259,8 @@ function CreateTableFromJson(data){
         var row = document.createElement('tr');
 
         for(var i=0;i<tableHeader.length;i++){
-            var temp = ddlDB.value + '.' + ddlEntities.value + '.' + tableHeader[i];
+            var temp = tableHeader[i];
+            console.log(temp);
             if(selectedAttributes.has(temp)){
                 anyBoxesChecked[i] = true;
             }
@@ -258,4 +294,65 @@ function CreateTableFromJson(data){
     document.getElementById('tableDisplayDiv').appendChild(table);
 }
 
+function ProcessArrayTable(arrayTable){
+    var res = arrayTable[0];
 
+    for(let i=1;i<arrayTable.length;i++){
+        var commonAtt = GetCommonAtt(res, arrayTable[i]);
+        res = JoinTable(res, arrayTable[i], commonAtt[0], commonAtt[1]);
+    }
+    
+    console.log(res);
+    CreateTableFromJson(res);
+}
+
+function JoinTable(tbl1, tbl2, commonAbb1, commonAbb2){
+    var qry = 'SELECT * FROM ? a, ? b where (';
+
+    for(let i=0;i<commonAbb1.length;i++){
+        if(i != 0){
+            qry += ' AND ';
+        }
+        qry += '(a.' + commonAbb1[i] + ' = b.' + commonAbb2[i] + ')';
+    }
+    qry += ')';
+
+    console.log(qry);
+    var res = alasql(qry,[tbl1,tbl2]);
+    console.log(res);
+    return res;
+}
+
+async function Temp(){
+    document.getElementById('tableDisplayDiv').innerHTML = "";
+    var lsText = await get_local_schema("dsid2","eid1");
+    var ls = get_XML_Dom_object(lsText);
+
+    var tb1 = await ProcessCSV(ls, "eid2");
+    var tb2 = await ProcessCSV(ls, "eid3");
+
+    var commonAtt = GetCommonAtt(tb1,tb2);
+    JoinTable(tb1, tb2, commonAtt[0], commonAtt[1]);
+
+    // var res = alasql('SELECT * FROM ? a, ? b where a.route_no = b.rt_no',[tb1,tb2]);
+    
+    // JoinTable(tb1, tb2, ['route_no'], ['route_no']);
+}
+
+function GetCommonAtt(tbl1, tbl2){
+    var com1 = [];
+    var com2 = [];
+
+    for(k in tbl1[0]){
+        for(l in tbl2[0]){
+            if(k in graph){
+                if(graph[k].includes(l)){
+                    com1.push(k);
+                    com2.push(l);
+                }
+            }
+        }
+    }
+
+    return [com1,com2];
+}
